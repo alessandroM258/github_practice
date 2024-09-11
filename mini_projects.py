27949a7cb85df7f56687401414bb1d5391d41758
alessandroM258 <144828778+alessandroM258@users.noreply.github.com>
2024-09-11 17:48:01 +0000

    gilbert

--- mini_projects.py
+++ mini_projects.py
@@ -1,1 +1,12 @@
+total_count = {"aluminum": 135, "plastic": 102, "paper": 213}
 
+def sortItems(itemString):
+  for items in itemString:
+    if items == "A":
+      total_count["aluminum"] += 1 
+    elif items == "P":
+     total_count["plastic"] += 1
+    elif items == "R":
+      total_count["paper"] += 1
+sortItems('AAPAARRRRPAAPPRRP')
+print(total_count)
